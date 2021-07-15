81463f5156d50608bcb4a88f8039f68f3dc891ec
Vasili Gulevich <vasili.gulevich@xored.com>
2021-07-15 15:04:46 +0700

    Extract a method for interactive use

--- build_branch.py
+++ build_branch.py
@@ -2,7 +2,7 @@ from os import environ
 
 from jenkins import Jenkins
 from git import Git
-from jenkins_util import build_branch
+from jenkins_util import build_branch as jenkins_build_branch
 
 from gitlab import Gitlab
 
@@ -12,18 +12,20 @@ itest_project = gitlab.projects.get(186)
 
 git = Git()
 
-branch = git.current_branch()
-# branch = 'bug/RM-23112--errorCode'
 
 jenkins = Jenkins('https://jenkins-itest.spirenteng.com/jenkins/', username='vgulevich',
                   password=environ['JENKINS_ITEST_TOKEN'])
 
 version = '8.6.0'
-git.update_branch(branch, 'v' + version)
-build_url = build_branch(jenkins, branch, installers=True, test=True, version=version)
-print(build_url)
 
-for merge in itest_project.mergerequests.list(source_branch=branch):
-    if merge.state == 'opened':
-        print(merge.web_url)
-        merge.notes.create(data={'body': "Started {}".format(build_url)})
+def build_branch():
+    branch = git.current_branch()
+    # branch = 'bug/RM-23112--errorCode'
+    git.update_branch(branch, 'v' + version)
+    build_url = jenkins_build_branch(jenkins, branch, installers=True, test=True, version=version)
+    print(build_url)
+    
+    for merge in itest_project.mergerequests.list(source_branch=branch):
+        if merge.state == 'opened':
+            print(merge.web_url)
+            merge.notes.create(data={'body': "Started {}".format(build_url)})
