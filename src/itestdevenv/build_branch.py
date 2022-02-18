fe580526d68450271ec44faa6bc9d3ad70c4c00f
Vasili Gulevich <vasili.gulevich@xored.com>
2022-02-18 14:37:58 +0700

    Better link to create a merge request

--- src/itestdevenv/build_branch.py
+++ src/itestdevenv/build_branch.py
@@ -1,10 +1,11 @@
 from os import environ
 from os.path import join
 from re import subn
+from urllib.parse import urlencode 
 
 from jenkins import Jenkins
 from .jenkins_util import build_branch as jenkins_build_branch
-from gitlab import Gitlab
+from gitlab import Gitlab #python-gitlab package
 
 from .git import Git
 from .maven import extract_version, remove_qualifier
@@ -28,23 +29,31 @@ def build_branch(branch=None):
         branch = git.current_branch()
     else:
         git.checkout(branch)
-    # branch = 'bug/RM-23112--errorCode'
     version = remove_qualifier(extract_version(main_pom))
     merge_requests = [merge for merge in itest_project.mergerequests.list(source_branch=branch) if merge.state == 'opened']
     branches = [merge.target_branch for merge in merge_requests]
+    release_branch = 'v' + version
     if not branches:
-        branches.append('v' + version)
+        branches.append(release_branch)
     git.update_branch(branch, *branches)
     build_url = jenkins_build_branch(jenkins, branch, docker=True, installers=True, test=True, version=version)
     print(build_url)
     
     for merge in merge_requests:
         print(merge.web_url)
-        print(merge.description)
         merge.notes.create(data={'body': "Started {}".format(build_url)})
         message = "Latest build: " + build_url
         description, count = subn(r'Latest build: https://[^\s]+(?=$|\n)', message, merge.description, count=1)
         if count == 0:
             description = merge.description + '\n\n' + message
+        print(description)
         merge.description = description
         merge.save()
+        
+    if not merge_requests:
+        description = "Latest build: " + build_url
+        query = { 'merge_request[source_branch]' : branch, 'merge_request[target_branch]' : release_branch, 'merge_request[description]' : description}
+        
+        #https://git-ito.spirenteng.com/itest/itest/-/merge_requests/new?merge_request%5Bsource_branch%5D=bug%2FITEST-20735--log4jcve
+        print(itest_project.web_url + '/-/merge_requests/new?' + urlencode(query))
+        
